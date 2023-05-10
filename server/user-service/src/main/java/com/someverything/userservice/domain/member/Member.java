2bfcf42305a61c8ebbe979f6fc033fa8df3e68ce
GMX\ZZ01WI766 <ksmhero@naver.com>
2023-05-10 14:13:21 +0900

    feat:user-service 엔티티 작성

--- server/user-service/src/main/java/com/someverything/userservice/domain/member/Member.java
+++ server/user-service/src/main/java/com/someverything/userservice/domain/member/Member.java
@@ -1,9 +1,25 @@
-package com.someverything.userservice.domain.member;
+package com.someverything.userservice.domain;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
+import javax.persistence.*;
+
+@Entity
+@Table(name = "Member")
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
 @Data
-public class Member {
+public class MemberEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String nickName;
+    private String accessToken;
+    private String email;
 
 }
-
